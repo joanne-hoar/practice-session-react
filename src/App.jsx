6abb38842198eb73b06c7118a37adf6ff81be142
Jo Hoar <joanne.hoar@robertson.brightspace.com>
2026-02-03 10:23:36 -0700

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import storeImage from '/cute-storefront.jpg'
+import Header from  './components/common/Header'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>        
-         <img src={storeImage} alt="Cute Storefront Logo" />
-         <button onClick={() => setCount((count) => count + 1)}>
+  /*
+   <button class="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        */
+  return (
+    <>
+      <Header />
+      <div>        
+         <img src={storeImage} alt="Cute Storefront Logo" />        
       </div>
     </>
   )
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <>
+        <h1>Joanne's Market</h1>
+        </>
+    );
+}
+
+export default Header;
