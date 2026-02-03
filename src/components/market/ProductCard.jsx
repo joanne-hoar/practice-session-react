5a5263bbc4b632633e1b8128f39afeffddac368f
Jo Hoar <joanne.hoar@robertson.brightspace.com>
2026-02-03 15:59:06 -0700

    add to cart event with handler

--- src/components/market/ProductCard.jsx
+++ src/components/market/ProductCard.jsx
@@ -2,10 +2,15 @@ import './ProductCard.css';
 
 function ProductCard({product}) {
 
+    function addToCart() {
+    alert(`Add ${product.name} to cart.`);
+  }
+
     return(
         <div class="product-card">
             <h3>{product.name}</h3>
             <img src={`products/${product.image}`} alt={product.name} />
+            <button class="add-btn" onClick={addToCart}>Add to Cart</button>
         </div>
     );
 }
